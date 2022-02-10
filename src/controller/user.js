f2213e0326f6af5466a2ddda75f86053715cf892
Felipe Martins <mfelipeluiz99@gmail.com>
2022-02-10 11:53:29 -0300

    Req1: Refac src/controller/user.js

--- src/controller/user.js
+++ src/controller/user.js
@@ -4,7 +4,7 @@ const createController = async (req, res) => {
     try {
         const { displayName, email, password, image } = req.body;
         await userService.createService({ displayName, email, password, image });
-        return res.status(201).json({ message: 'User successfully registered' });
+        return res.status(201).json({ token: 'User successfully registered' });
        } catch (error) {
         res.status(500).json({ message: error.message });
        }
