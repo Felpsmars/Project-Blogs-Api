59758a41f9383ad94db346dab23ae6efc8d07125
Felipe Martins <mfelipeluiz99@gmail.com>
2022-02-10 14:45:52 -0300

    Req2: creating middlewares/isEmailEmpty.js and middlewares/isPasswordEmpty.js

---
+++ src/middlewares/isPasswordEmpty.js
@@ -0,0 +1,13 @@
+const isPasswordEmpty = (req, res, next) => {
+    const { password } = req.body;
+
+    if (password === '') {
+        return res.status(400).json({
+            message: '"password" is not allowed to be empty',
+        });
+    }
+
+    next();
+};
+
+module.exports = isPasswordEmpty;
