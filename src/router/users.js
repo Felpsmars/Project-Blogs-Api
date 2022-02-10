c71c6536481cae1d04e9f219bfc15e0ec741d91b
Felipe Martins <mfelipeluiz99@gmail.com>
2022-02-09 23:09:00 -0300

    Req1: some error fixes on src/router/users.js

--- src/router/users.js
+++ src/router/users.js
@@ -2,12 +2,22 @@ const express = require('express');
 const UserController = require('../controller/user');
 const isDisplayNameLengthValid = require('../middlewares/isDisplayNameLengthValid');
 const isDisplayNameOnBody = require('../middlewares/isDisplayNameOnBody');
+const isEmailFormatValid = require('../middlewares/isEmailFormatValid');
+const isEmailOnBody = require('../middlewares/isEmailOnBody');
+const isPasswordLengthValid = require('../middlewares/isPasswordLengthValid');
+const isPasswordOnBody = require('../middlewares/isPasswordOnBody');
+const isEmailAlreadyExist = require('../middlewares/isEmailAlreadyExist');
 
 const routes = express.Router();
 
 routes.post('/',
     isDisplayNameOnBody,
     isDisplayNameLengthValid,
+    isEmailOnBody,
+    isEmailFormatValid,
+    isPasswordOnBody,
+    isPasswordLengthValid,
+    isEmailAlreadyExist,
     UserController.createController);
 
 module.exports = routes;
