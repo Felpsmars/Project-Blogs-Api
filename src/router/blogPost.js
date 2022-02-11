56c088df608e0a401c76d0edafc259d12859a2e4
Felipe Martins <mfelipeluiz99@gmail.com>
2022-02-11 14:45:25 -0300

    Req7: Creating blogPost router

---
+++ src/router/blogPost.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const blogPost = require('../controller/blogPost');
+const isCategoryIdOnBody = require('../middlewares/isCategoryIdOnBody');
+const isContentOnBody = require('../middlewares/isContentOnBody');
+const isTitleOnBody = require('../middlewares/isTitleOnBody');
+const isCategoryExist = require('../middlewares/isCategoryExist');
+const validatedToken = require('../middlewares/isTokenValid');
+
+const routes = express.Router();
+
+routes.post('/',
+    validatedToken,
+    isCategoryIdOnBody,
+    isContentOnBody,
+    isTitleOnBody,
+    isCategoryExist,
+    blogPost.createBlogPostController);
+
+module.exports = routes;
